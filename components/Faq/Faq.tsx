da82f8c7bb2fa4b5c093c923168445f9938c0b53
ashworks1706 <asriv132@asu.edu>
2025-04-11 11:45:15 -0700

    faq

--- components/Faq/Faq.tsx
+++ components/Faq/Faq.tsx
@@ -8,10 +8,10 @@ const HacksFaq = [
     question: "Who can participate?",
     answer: (
       <>
-        Innovation Hacks 2025 is open to a diverse range of participants, including
-        students, developers, designers, and tech enthusiasts. However, participants
-        must be part of Arizona State University. Whether you're a beginner or an
-        experienced coder, there's a place for you here!
+        Innovation Hacks 2025 is open to a diverse range of participants,
+        including students, developers, designers, and tech enthusiasts.
+        However, participants must be part of Arizona State University. Whether
+        you're a beginner or an experienced coder, there's a place for you here!
       </>
     ),
   },
@@ -19,10 +19,10 @@ const HacksFaq = [
     question: "I've never been to a hackathon before. Should I participate?",
     answer: (
       <>
-        Congratulations on considering your first hackathon! Innovation Hacks 2025 is a
-        perfect starting point. We welcome participants of all skill levels and
-        provide mentorship throughout the event. It's an excellent opportunity to
-        learn, collaborate, and create something amazing!
+        Congratulations on considering your first hackathon! Innovation Hacks
+        2025 is a perfect starting point. We welcome participants of all skill
+        levels and provide mentorship throughout the event. It's an excellent
+        opportunity to learn, collaborate, and create something amazing!
       </>
     ),
   },
@@ -30,7 +30,9 @@ const HacksFaq = [
     question: "Do you provide travel reimbursement?",
     answer: (
       <>
-        No, Innovation Hacks 2025 does not offer travel reimbursement for participants. Participants are responsible for their travel expenses, and we recommend planning accordingly.
+        No, Innovation Hacks 2025 does not offer travel reimbursement for
+        participants. Participants are responsible for their travel expenses,
+        and we recommend planning accordingly.
       </>
     ),
   },
@@ -38,9 +40,9 @@ const HacksFaq = [
     question: "Do I need to know how to code?",
     answer: (
       <>
-        Yes, Innovation Hacks 2025 welcomes participants from diverse backgrounds,
-        including those new to coding. We encourage learning and provide resources
-        and mentorship to help you succeed!
+        Yes, Innovation Hacks 2025 welcomes participants from diverse
+        backgrounds, including those new to coding. We encourage learning and
+        provide resources and mentorship to help you succeed!
       </>
     ),
   },
@@ -48,10 +50,10 @@ const HacksFaq = [
     question: <>What will I get after attending Innovation Hacks 2025?</>,
     answer: (
       <>
-        Innovation Hacks 2025 offers a dynamic platform for learning and collaboration.
-        You'll gain hands-on experience, network with industry professionals,
-        receive mentorship, and have the chance to win exciting prizes. Plus,
-        you'll create lasting connections in the tech community.
+        Innovation Hacks 2025 offers a dynamic platform for learning and
+        collaboration. You'll gain hands-on experience, network with industry
+        professionals, receive mentorship, and have the chance to win exciting
+        prizes. Plus, you'll create lasting connections in the tech community.
       </>
     ),
   },
@@ -68,8 +70,8 @@ const HacksFaq = [
     question: <>Are team members from other colleges allowed? </>,
     answer: (
       <>
-        No, Innovation Hacks 2025 welcomes participants from diverse backgrounds,
-        only from Arizona State University.
+        No, Innovation Hacks 2025 welcomes participants from diverse
+        backgrounds, only from Arizona State University.
       </>
     ),
   },
@@ -77,33 +79,43 @@ const HacksFaq = [
     question: <>What if I do not have a team? </>,
     answer: (
       <>
-        We have a great community on Discord. Reach out to other
-        innovators, collaborate, communicate, and make things possible! 🤝
+        We have a great community on Discord. Reach out to other innovators,
+        collaborate, communicate, and make things possible! 🤝
       </>
     ),
   },
   {
     question: <>Have more questions? </>,
-    answer: <>Feel free to write to us at help@innovationhacks.dev or create a ticket🎫 on <a className="text-blue-400 underline" href="https://discord.gg/the-software-developers-association-762811961238618122" target="_blank">discord</a>
-    </>,
+    answer: (
+      <>
+        Feel free to write to us at help@innovationhacks.dev or create a
+        ticket🎫 on{" "}
+        <a
+          className="text-blue-400 underline"
+          href="https://discord.gg/the-software-developers-association-762811961238618122"
+          target="_blank"
+        >
+          discord
+        </a>
+      </>
+    ),
   },
 ];
 
 export default function FAQ() {
   return (
-        <div className="mt-32 w-full items-center justify-center flex flex-col gap-12  p-10 ">
-          <div className="text-center   flex flex-col">
-            <div className="my-8 text-white text-xl sm:text-2xl md:text-4xl">
-              <h2 className="text-2xl text-center font-bold text-white dark:text-white md:text-3xl lg:text-4xl font-logo">
-                Frequently Asked Questions
-              </h2>
-            </div>
-
-          </div>
-          <div className="flex w-full faqContainer1  flex-col items-center justify-center">
-            <Disclosures />
-          </div>
+    <div className="mt-32 w-full items-center justify-center flex flex-col gap-12  p-10 ">
+      <div className="text-center   flex flex-col">
+        <div className="my-8 text-white text-xl sm:text-2xl md:text-4xl">
+          <h2 className="text-2xl text-center font-bold text-white dark:text-white md:text-3xl lg:text-4xl font-logo">
+            Frequently Asked Questions
+          </h2>
         </div>
+      </div>
+      <div className="flex w-full faqContainer1  flex-col items-center justify-center">
+        <Disclosures />
+      </div>
+    </div>
   );
 }
 
@@ -119,19 +131,20 @@ export function Disclosures({ full = false }) {
       {HacksFaq.map((item, i) => (
         <div
           key={String(i)}
-          className={`mx-auto text-lg ${full ? "" : "max-w-5xl"}`}
+          className={`mx-auto  cursor-pointer text-lg ${
+            full ? "" : "max-w-5xl"
+          }`}
         >
           {/* rome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-<div
+          <div
             className="flex w-full items-start justify-between py-6 text-left text-gray-400"
             onClick={() => handleToggle(i)}
           >
-            <span className="font-medium text-white dark:text-white">
+            <span className="font-medium w-[62vw] text-white dark:text-white">
               {item.question}
             </span>
             <span className="ml-6 flex h-7 items-center">
-              {/* rome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-<svg
+              <svg
                 className={`arrow-down h-6 w-6 transform duration-300 ${
                   openIndex === i ? "rotate-180" : "rotate-0"
                 }`}
